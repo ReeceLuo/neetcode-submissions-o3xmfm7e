eaec4029ae671366855db545e7aaac42e05d8133
ReeceLuo <167668969+ReeceLuo@users.noreply.github.com>
2026-06-22 03:47:45 -0500

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        # brute force - check every subarray
+        # use dynamic programming
+        # Kadane's algorithm
+
+        maxSum = nums[0]
+        curSum = 0
+        for num in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += num
+            maxSum = max(maxSum, curSum)
+
+        return maxSum
+
+
+        
+
+
+            
