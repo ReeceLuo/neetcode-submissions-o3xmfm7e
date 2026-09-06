c9b69711056b97345bafc699db1c33d2015b27a4
ReeceLuo <167668969+ReeceLuo@users.noreply.github.com>
2026-09-06 14:17:49 -0500

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # check if a string contains a permutation of another string
+
+        counts1 = {}
+        for char in s1:
+            counts1[char] = counts1.get(char, 0) + 1
+
+        for i, char in enumerate(s2):
+            if char in counts1:
+                r = i
+                counts2 = {}
+                while r < len(s2):
+                    counts2[s2[r]] = counts2.get(s2[r], 0) + 1
+                    if counts2[s2[r]] > counts1.get(s2[r], 0):
+                        break
+                    if counts2 == counts1:
+                        return True
+                    r += 1
+
+        return False
